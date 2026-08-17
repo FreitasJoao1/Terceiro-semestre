f7774adb362e3897ce892f1e9344005deaf76985
Freitass <fjoaopedro03@gmail.com>
2026-08-17 14:57:37 -0300

    Add files via upload

--- Desenvolvimento_Sistemas/Aula17_08/Questao06.java
+++ Desenvolvimento_Sistemas/Aula17_08/Questao06.java
@@ -1,13 +1,24 @@
 import java.util.Scanner;
 
 public class Questao06 {
-    Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        
+     
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite o 1º numero: ");
+        double n1 =sc.nextDouble();
+        
+        System.out.println("Digite o 2º numero: ");
+        double n2 =sc.nextDouble();
+        
+        System.out.println("Digite a operção que deseja: ");
+        String op =sc.nextLine();
+        Double res = sc.nextDouble();
+        
+        if(op.equals("+")) {
+            res = (n1 + n2);
+            System.out.println("");
+        }
+    }
 
-    System.out.println("Digite o 1º numero: ");
-    double n1 =sc.nextDouble();
-
-    System.out.println("Digite o 2º numero: ");
-    double n2 =sc.nextDouble();
-
-    System.out.println("Digite a operção que deseja: ");
 }
